a186a40387a169977c8a5f3108754ee1f9bf2d1a
Bob Buehler <bob.buehler@gmail.com>
2016-04-23 15:39:40 -0500

    XState

--- Games/Spiders/State.cs
+++ Games/Spiders/State.cs
@@ -5,7 +5,79 @@ using System.Text;
 
 namespace Joueur.cs.Games.Spiders
 {
-    class State
+    public enum XSpiderType
     {
+        BroodMother,
+        Cutter,
+        Spitter,
+        Weaver
+    }
+
+    public class XBase
+    {
+        public string Id;
+        public override int GetHashCode()
+        {
+ 	         return Id.GetHashCode();
+        }
+    }
+
+    public class XPlayer : XBase
+    {
+        public int ID;
+        public XSpider BroodMother;
+        public IEnumerable<XSpider> Cutters;
+        public IEnumerable<XSpider> Spitters;
+        public IEnumerable<XSpider> Weavers;
+    }
+
+    public class XSpider : XBase
+    {
+        // Spider
+        public XSpiderType Type;
+        public XNest Nest;
+        public XPlayer Owner;
+
+        // BroodMother
+        public double Eggs;
+        public int Health;
+
+        // Spiderling
+        public IEnumerable<XSpider> Coworkers;
+        public XWeb MovingOnWeb;
+        public XNest MovingToNest;
+        public double WorkRemaining;
+
+        // Cutter
+        public XWeb CuttingWeb;
+
+        // Spitter
+        public XNest SpittingToNest;
+
+        // Weaver
+        public XWeb StrengtheningWeb;
+        public XWeb WeakeningWeb;
+    }
+
+    public class XNest : XBase
+    {
+        public Point Location;
+        public IEnumerable<XWeb> Webs;
+        public IEnumerable<XSpider> Spiders;
+    }
+
+    public class XWeb : XBase
+    {
+        public double Length;
+        public int Load;
+        public XNest NestA;
+        public XNest NestB;
+        public IEnumerable<XSpider> Spiders;
+        public int Strength;
+    }
+
+    public class XState
+    {
+        // Players, Nests, Webs
     }
 }
